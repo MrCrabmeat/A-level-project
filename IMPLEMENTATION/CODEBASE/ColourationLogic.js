73cf4c1f682cd12f71ff7967400cc38b3ea12304
Ashton.N <ashton29th@gmail.com>
2026-01-06 18:29:07 +0000

    Nearing end of template, just requires theme changing scripts next

--- IMPLEMENTATION/CODEBASE/ColourationLogic.js
+++ IMPLEMENTATION/CODEBASE/ColourationLogic.js
@@ -19,13 +19,61 @@ const darkTheme = {
     TBcolour: '#E4DDD8',
     MPtext: '#EBFAFF',
     BGcolour:
-    "100deg, #020229 0%, #34013C 33%, #814508 100%",
+    "70deg, #020229 0%, #34013C 33%, #814508 100%",
+    accessory: 'white',
+    themeID: 'DARK'
 }
 const lightTheme = {
     TBvectorColour: '#EBFAFF',
     TBcolour: '#333232',
     MPtext: '#0B0B0B',
     BGcolour:
-    "100deg, #9D82DF 0%, #D86FEC 33%, #EC8F30 100%",
+    "70deg, #9D82DF 0%, #D86FEC 33%, #EC8F30 100%",
+    // error previously setting the gradient to 100deg,
+    // the design uses 70 (-100) degrees
+    accessory: 'black',
+    themeID: 'LIGHT'
+    // themeID allows changeTheme() to identify the current website
+    // theme to allow it to correctly switch to another theme.
 }
+let currentthemeID = ''
+// Sets a global variable to allow functions to identify the current
+// theme being used. It uses a string for readability and scalability due
+// to allowing other themes to be added later if needs be.
 
+// To apply the colour changes, the classes of
+// Taskbar buttons and text need to change.
+// The background should update with the
+// BGcolour needed.
+function themeLoader(theme) {
+    document.documentElement.style.setProperty(
+        '--TBvectorColour', theme.TBvectorColour);
+    document.documentElement.style.setProperty(
+        '--TBcolour', theme.TBcolour);
+    document.documentElement.style.setProperty(
+        '--MPtext', theme.MPtext);
+    document.documentElement.style.setProperty(
+        '--BGcolour',  `linear-gradient(${theme.BGcolour})`)
+    //`linear-gradient(${theme.BGcolour}) turns the var() in the CSS
+    // into a working function, though the CSS IDE outputs "unresolved",
+    // the function doesn't initially show due to requiring HTML loading.
+    document.documentElement.style.setProperty(
+        '--accessory', theme.accessory);
+    currentthemeID = theme.themeID;
+    // Sets the global theme ID variable to the theme selected to update
+    // to the new  theme being used, keeping functions up to date.
+}
+themeLoader(lightTheme);
+// Initially loads a default theme for the website
+function changeTheme() {
+    if (currentthemeID === 'DARK') {
+        themeLoader(lightTheme);}
+
+    else if (currentthemeID === 'LIGHT') {
+        themeLoader(darkTheme);}
+    else {
+        themeLoader(darkTheme)
+        reportError("Theme not found, setting default theme");}
+    // If no themeID is identified, the function falls back onto the
+    // default theme and outputs and error message.
+}
